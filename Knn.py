ed27a9ea02ad27de513f9bd95a28bfce9eb7fab0
MacAir <macair@MacAirdeMacBook-Air.local>
2018-01-18 11:40:21 +0800

    add handwritingTest

--- Knn.py
+++ Knn.py
@@ -1,4 +1,5 @@
 from numpy import *
+import os
 import matplotlib
 import matplotlib.pylab as plt
 
@@ -44,6 +45,7 @@ def autoNorm(dataSet):
     normDataSet = normDataSet / tile(ranges, (m, 1))
     return normDataSet, ranges, minVals
 
+
 def datingClassTest():
     hoRatio = 0.10
     datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
@@ -59,4 +61,41 @@ def datingClassTest():
     print('the total error rate is: %f' % (errorCount/numTestVecs))
 
 
-datingClassTest()
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, i*32+j] = int(lineStr[j])
+    return returnVect
+
+
+def handWritingClassTest():
+    hwLabels = []
+    trainingFileList = os.listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        filenameStr = trainingFileList[i]
+        fileStr = filenameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % filenameStr)
+    testFileList = os.listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        filenameStr = testFileList[i]
+        fileStr = filenameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % filenameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print('the classifier came back with: %d, the real answer is : %d' % (classifierResult, classNumStr))
+        if classifierResult != classNumStr:
+            errorCount=errorCount+1.0
+    print('\nthe total number of errors is: %d' % errorCount)
+    print('\nthe total error rate is : %f' % (errorCount/float(mTest)))
+
+
+handWritingClassTest()
